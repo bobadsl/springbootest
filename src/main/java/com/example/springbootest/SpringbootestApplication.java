49caacf79e39f297bda909867b5c70c54f24b1ac
Bouchaib <bouchkhaf@gmail.com>
2023-03-16 15:26:45 +0100

    Ajout des commentaires dans les classes

--- src/main/java/com/example/springbootest/SpringbootestApplication.java
+++ src/main/java/com/example/springbootest/SpringbootestApplication.java
@@ -1,8 +1,20 @@
 package com.example.springbootest;
 
-import org.springframework.boot.SpringApplication;
+/**
+ * Permettent d'utiliser les annotations pour configurer la classe
+ * SpringbootestApplication
+ *
+ * dependance dans le build.gradle :
+ *    	implementation 'org.springframework.boot:spring-boot-starter'
+ */
+
+import org.springframework.boot.SpringApplication; // Permettra de lancer l'application avec la méthode run()
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * Annotation qui indique au framework Spring Boot que c'est un point d'entrée de l'application
+ */
 @SpringBootApplication
 public class SpringbootestApplication {
 
--- src/test/java/com/example/springbootest/SpringbootestApplicationTests.java
+++ src/test/java/com/example/springbootest/SpringbootestApplicationTests.java
@@ -1,5 +1,14 @@
 package com.example.springbootest;
 
+
+
+/**
+ * Permettent d'utiliser les annotations pour configurer la classe
+ * SpringbootestApplicationTests
+ *
+ * dependance dans le build.gradle :
+ *    testImplementation 'org.springframework.boot:spring-boot-starter-test'
+ */
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.Assert;
@@ -7,9 +16,18 @@ import org.springframework.util.Assert;
 import java.util.ArrayList;
 import java.util.List;
 
+
+/**
+ * Annotation qui indique au framework Spring Boot que c'est une classe de test
+ */
 @SpringBootTest
 class SpringbootestApplicationTests {
 
+
+	/**
+	 * Annotation qui indique à Spring Boot que c'est une méthode de test
+	 * La méthode test() apparaitra dans le rapport de tests
+	 */
 	@Test
 	void test() {
 		Assert.isTrue((4/2) == 2, "Non égaux");
